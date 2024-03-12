cb116da4987906feb0b14ad9eef370600501f7f9
Vlad <wbcom9543@gmail.com>
2024-03-12 11:27:52 +0200

    first commit

---
+++ MultiThreading/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SaveAsThread saveAsThread1;
+        SaveAsThread saveAsThread2;
+        SaveAsThread saveAsThread3;
+        InitializeArThread initialize1;
+        InitializeArThread initialize2;
+        InitializeArThread initialize3;
+        public int[] array2;
+        public int[] array3;
+        public int[] array1;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initialize1.Stop();
+            initialize2.Stop();
+            initialize3.Stop();
+            saveAsThread1.Stop();
+            saveAsThread2.Stop();
+            saveAsThread3.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            array1 = new int[10000000];
+            array2 = new int[10000000];
+            array3 = new int[10000000];
+            initialize1 = new InitializeArThread(array1);
+            initialize2 = new InitializeArThread(array2);
+            initialize3 = new InitializeArThread(array3);
+            saveAsThread1 = new SaveAsThread(array1, "1.txt");
+            saveAsThread2 = new SaveAsThread(array2, "2.txt");
+            saveAsThread3 = new SaveAsThread(array3, "3.txt");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            initialize1.Start();
+            initialize2.Start();
+            initialize3.Start();
+            saveAsThread1.Start();
+            saveAsThread2.Start();
+            saveAsThread3.Start();
+        }
+    }
+}
---
+++ MultiThreading/SaveAsThread.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    //public delegate void ArrToFileDelegate(int[] array, string fileName);
+    /* class SaveAsThread
+     {
+         private Thread thread;
+         private ArrToFileDelegate threadStart;
+         private int[] array;
+         private string fileName;
+         public SaveAsThread(ArrToFileDelegate threadStart, int[] array, string fileName)
+         {
+
+             this.threadStart = threadStart;
+             this.array = array;
+             this.fileName = fileName;
+             thread = new Thread(new ThreadStart(ThreadMethod));
+         }
+
+         public void Start()
+         {
+             thread.Start();
+         }
+          private void ThreadMethod()
+         {
+             threadStart.Invoke(array, fileName);
+         }
+     }*/
+    public class SaveAsThread
+    {
+        private int[] array;
+        private string fileName;
+        private Thread thread;
+        public SaveAsThread(int[] array, string fileName)
+        {
+            this.array = array;
+            this.fileName = fileName;
+
+        }
+
+        public void SaveToFile()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            StreamWriter writer = new StreamWriter(fileName);
+            stopwatch.Start();
+            foreach (int item in array)
+            {
+                writer.Write(item + "; ");
+            }
+            stopwatch.Stop();
+            writer.Close();
+            Console.WriteLine($"масив було сбережено у файл: {fileName}");
+            Console.WriteLine("час виконання: " + stopwatch.Elapsed);
+        }
+        public void Start()
+        {
+            if (thread == null || thread.ThreadState == System.Threading.ThreadState.Unstarted || thread.ThreadState == System.Threading.ThreadState.Stopped)
+            {
+                thread = new Thread(SaveToFile);
+                thread.Start();
+            }
+            else
+            {
+                Console.WriteLine("Потік вже запущено");
+            }
+        }
+        public void Stop()
+        {
+            if (thread != null)
+            {
+                if (thread.IsAlive)
+                {
+                    thread.Interrupt();
+                }
+                else
+                {
+                    Console.WriteLine("Потік не запущено");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Потік не ініціалізовано");
+            }
+        }
+    }
+    public class InitializeArThread
+    {
+        private int[] array;
+        private Thread thread;
+        Random rand;
+        public InitializeArThread(int[] array)
+        {
+            rand = new Random();
+            this.array = array;
+        }
+
+        public void Init()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(10566);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("масив було ініціалізовано числами");
+            Console.WriteLine("час виконання: " + stopwatch.Elapsed);
+
+        }
+        public void Start()
+        {
+
+            if (thread == null || thread.ThreadState == System.Threading.ThreadState.Unstarted || thread.ThreadState == System.Threading.ThreadState.Stopped)
+            {
+                thread = new Thread(Init);
+                thread.Start();
+            }
+            else
+            {
+                Console.WriteLine("Потік вже запущено");
+            }
+        }
+        public void Stop()
+        {
+            if (thread != null)
+            {
+                if (thread.IsAlive)
+                {
+                    thread.Interrupt();
+                }
+                else
+                {
+                    Console.WriteLine("Потік не запущено");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Потік не ініціалізовано");
+            }
+        }
+    }
+}
